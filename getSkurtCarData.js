f7cf89faa47f8ac46f61c2ce0b517d882cd430a6
phaedraa <phaedra.a.r@gmail.com>
2017-02-26 17:02:33 -0800

    Test fetching all data

--- getSkurtCarData.js
+++ getSkurtCarData.js
@@ -1,11 +1,12 @@
 function getSkurtCarData(id) {
-    console.log('in getSkurtCarData');
+  console.log('in getSkurtCarData');
   var http = require("http");
-  var url = "http://skurt-interview-api.herokuapp.com/carStatus/{id}";
-  
+  var url = "http://skurt-interview-api.herokuapp.com/carStatus/" + id;
+  console.log('url: ', url);
   // get is a simple wrapper for request()
   // which sets the http method to GET
   var request = http.get(url, function (response) {
+    console.log('in request');
     // data is streamed in chunks from the server
     // so we have to handle the "data" event    
     var buffer = "", 
@@ -15,7 +16,7 @@ function getSkurtCarData(id) {
     response.on("data", function (chunk) {
       buffer += chunk;
     }); 
-  
+    console.log('buffer: ', buffer);
     response.on("end", function (err) {
       // finished transferring data
       // dump the raw data
@@ -25,9 +26,12 @@ function getSkurtCarData(id) {
   
       // extract the data
       console.log("Car 1: " + data);
-       return data;
+      return data;
     }); 
   });
+
+  console.log('request', request);
+  //console.log('data', data);
 }
 
 module.exports = getSkurtCarData;
--- notifyIfDriverOutOfRange.js
+++ notifyIfDriverOutOfRange.js
@@ -2,36 +2,19 @@ const NUM_CARS = 11;
 console.log('here');
 var getSkurtCarData = require('./getSkurtCarData.js');
 var isCarOutsideRange = require('./isCarOutsideRange.js');
+var testNodeRequest = require('./testNodeRequest.js');
 console.log('here2');
 
 function notifyIfDriverOutOfRange() {
   console.log('notifyIfDriverOutOfRange');
-  //var car_ids = getCars(NUM_CARS);
   var carLocData = [];
   for (var car_id = 1; car_id <= NUM_CARS; car_id++) {
-    var car_data = getSkurtCarData(car_id);
-    //carLocData[] = {car_id: getSkurtCarData(car_id)};
+    var car_data = testNodeRequest(car_id);
     if (isCarOutsideRange(car_data)) {
       // send email
       console.log('now send email!');
     }
   }
-
-  //var carsOutsideRange = [];
-  //for (var datum = 0; datum < NUM_CARS; datum++) {
-  //  if (isCarOutsideRange(carLocData[datum])) {
-  //    carsOutsideRange[] = {carLocData[datum].}
-  //  }
-  //}
-
-  //function getCars(num_cars) {
-  //  var cars = [];
-  //  car_id = 1;
-  //  while (car_id <= num_cars) {
-  //    cars[] = car_id;
-  //    car_id++;
-  //  }
-  //}
 }
 
 notifyIfDriverOutOfRange();
